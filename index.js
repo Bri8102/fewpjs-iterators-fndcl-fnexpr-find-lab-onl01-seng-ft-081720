e6e53dd0792bd6ae0e01ea85a359b6329fae8112
Bri8102 <damioniru16@gmail.com>
2020-12-10 14:45:36 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+   const result = array.find(function(e) {
+     return e.result === 'W'
+   })
+   return !!result ? result.year : result
+  }
